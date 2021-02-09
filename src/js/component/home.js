2cc5f9e70ced91f1a136cd299dd2a28995ea57b7
Leonelaam <leoneamaya@hotmail.com>
2021-02-09 02:46:55 +0000

    counter

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,40 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div
+				className="container"
+				style={{
+					backgroundColor: "black",
+					fontSize: "70px",
+					color: "white",
+					borderRadius: "15px",
+					padding: "20px"
+				}}>
+				<div className="row">
+					<div className="col-4">
+						<i className="fas fa-stopwatch" />
+					</div>
+					<div className="col-2">
+						<div className="digito3">{props.hora}</div>
+					</div>
+					<div className="col-2">
+						<div className="digito2">{props.minuto}</div>
+					</div>
+					<div className="col-2">
+						<div className="digito1">{props.segundo}</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
+
+Home.propTypes = {
+	hora: PropTypes.number,
+	minuto: PropTypes.number,
+	segundo: PropTypes.number
+};
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,25 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+//ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let contador = 0;
+let minuto = 0;
+let hora = 0;
+setInterval(function() {
+	var one = Math.floor(contador / 1);
+
+	contador++;
+
+	if (contador > 59) {
+		contador = 0;
+		minuto = minuto + 1;
+	} else if (minuto > 59) {
+		hora = hora + 1;
+		minuto = 0;
+	}
+	ReactDOM.render(
+		<Home segundo={contador} minuto={minuto} hora={hora} />,
+		document.querySelector("#app")
+	);
+}, 1000);
